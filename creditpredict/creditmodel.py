896fcd873e3a08d389d128f9eb352afbcf44c1b3
rajap20 <79008910+rajap20@users.noreply.github.com>
2021-12-07 03:07:54 +0530

    Update creditmodel.py

--- creditpredict/creditmodel.py
+++ creditpredict/creditmodel.py
@@ -7,11 +7,11 @@ from io import BytesIO
 import requests
 warnings.filterwarnings('ignore')
 
-class CreditPredictionModel ():
+class CreditPredictionModel():
     
     def __init__(self, pipeline, all_features, cat_features, num_features, accuracy):
         self.pipeline = pipeline
         self.all_features = all_features
         self.cat_features = cat_features
         self.num_features = num_features
-        self.accuracy = accuracy
+        self.cutoff = cutoff
